f6b5c8c920aab43b2cfb38bc6651659d75f94fa4
raphaeldjangmah <rdosro-agbo@st.ug.edu.gh>
2022-08-28 12:58:13 +0000

    User_Order_Add_Dashboard

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace CSharp_Invent_HUB
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Customer());
+            Application.Run(new User_Dashbord());
         }
     }
 }

e7c13c3ac0000c8510cbbc5e0fddeae8bfdc77e1
raphaeldjangmah <rdosro-agbo@st.ug.edu.gh>
2022-08-28 03:46:52 +0000

    Customer table furnished

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace CSharp_Invent_HUB
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Category_Table());
+            Application.Run(new Customer());
         }
     }
 }

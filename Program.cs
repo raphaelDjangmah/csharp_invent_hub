f34def8f54f481afa1ec4d1cb0aa7314b78366e6
raphaeldjangmah <rdosro-agbo@st.ug.edu.gh>
2022-08-28 03:12:44 +0000

    Category table furnished

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using CSharp_Invent_HUB.csharp_invent_hub.Admin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +18,7 @@ namespace CSharp_Invent_HUB
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Signup_Form());
+            Application.Run(new Category_Table());
         }
     }
 }
